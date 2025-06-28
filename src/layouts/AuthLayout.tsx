65de034423a815b78b383443ae1d1d456586b9a3
ThuanNguyenITVN <nguyenminhthuan2003st@gmail.com>
2025-06-28 21:48:02 +0700

    Feat: Enhance UI and add topics route
    feat:
    - Change background colors for AuthLayout and Register components to `bg-content1`
    - Add topics route with private access
    
    fix:
    - Update UpVote component to use HeroUI Button component and improve layout
    - Update caniuse-lite and rollup dependencies

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -5,7 +5,7 @@ const AuthLayout: React.FC<LayoutProps> = ({ children }) => {
 
     return (
         <div className='flex justify-center flex-col !mx-auto !h-screen bg-[url(./bg-login.webp)]'>
-            <div className='bg-content2 !h-3/4 w-[45%] mx-auto rounded' data-theme={"black"}>
+            <div className='bg-content1 !h-3/4 w-[45%] mx-auto rounded' data-theme={"black"}>
                 <h1 className="text-6xl mb-4 text-center text-red-600 mt-8">Katz Dev</h1>
                 <div className='text-center text-sm font-bold mb-4'> A place to share knowledge and better understand the world</div>
                 {children}
