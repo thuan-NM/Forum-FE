b7a15063ece14f0e6921b711a956b327362b535b
ThuanNguyenITVN <nguyenminhthuan2003st@gmail.com>
2025-08-13 17:18:34 +0700

    ✨ feat(profile): enhance user bio with image upload and topic prediction for questions
    - 【feat】: Implemented image upload functionality in user bio using `useUploadImages` hook
       - Allows users to include images in their bio content
       - Utilizes `processContentWithUploads` to handle image uploads and replace them with appropriate references
    - 【feat】: Added topic prediction functionality when creating questions
       - Uses `predictTopic` to suggest relevant topics based on question content
       - Implements a confirmation modal to allow users to select a predicted topic if no topic is initially selected
    - 【refactor】: Refactored user update logic into `useUpdateUser` hook for better code organization
    - 【refactor】: Improved modal structure for question creation, using `Modal` component for better UI
    - 【fix】: Fixed bug where topic was not being properly selected when creating questions
    - 【style】: Updated button styles and alert messages for improved user experience

--- src/services/PredictQuestionTopic.ts
+++ src/services/PredictQuestionTopic.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 const apiUrl = import.meta.env.VITE_PREDICT_API;
 const PredictQuestionTopic = async (data: string) => {
+  // const response = await axios.post(
+  //   `${apiUrl}/predict-fasttext`,
+  //   { text: data },
+  // );
   const response = await axios.post(
-    `${apiUrl}/predict-fasttext`,
+    `${apiUrl}/predict-fasttext-top-k`,
     { text: data },
   );
   return response.data;
